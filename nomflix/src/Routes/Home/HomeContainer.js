2435bd60a1f7bee0fb79a7d4eb507387e282e0da
doyeon <doyeon311@gmail.com>
2021-07-23 00:10:26 +0900

    #6.1 HomePresenter and Section Components

--- nomflix/src/Routes/Home/HomeContainer.js
+++ nomflix/src/Routes/Home/HomeContainer.js
@@ -40,7 +40,6 @@ class Home extends React.Component {
 
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
-    console.log(this.state);
     return (
       <HomePresenter
         nowPlaying={nowPlaying}
